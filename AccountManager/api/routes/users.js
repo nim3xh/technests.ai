d8441697d09575559d0a45f419171860aff48307
nim3xh <nim3xh@gmail.com>
2024-09-24 06:25:49 +0530

    Refactor user routes to use POST method for saving users

--- AccountManager/api/routes/users.js
+++ AccountManager/api/routes/users.js
@@ -2,6 +2,6 @@ const express = require("express");
 const usersController = require("../controllers/user.controller");
 const router = express.Router();
 
-router.get("/", usersController.index);
+router.post("/", usersController.save);
 
 module.exports = router;
