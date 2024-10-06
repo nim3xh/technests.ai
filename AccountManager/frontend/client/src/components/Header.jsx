c2c8633f4ac9d56cf37f74a94ce96bbd83cee824
nim3xh <nim3xh@gmail.com>
2024-10-07 02:42:37 +0530

    Refactor Header component to include theme toggle button and user sign out functionality

--- AccountManager/frontend/client/src/components/Header.jsx
+++ AccountManager/frontend/client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar, TextInput,Button } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaMoon, FaSun } from "react-icons/fa";
@@ -7,34 +7,78 @@ import { toggleTheme } from "../redux/theme/themeSlice";
 import { signoutSuccess } from "../redux/user/userSlice";
 import { useEffect, useState } from "react";
 
+const BaseURL = import.meta.env.VITE_BASE_URL;
+
 export default function Header() {
-    const { theme } = useSelector((state) => state.theme);
-    const dispatch = useDispatch();
-    return (
-      <Navbar
-        className="border-b-2 sticky top-0 z-50 justify-between"
-        fluid
-        rounded
+  const { theme } = useSelector((state) => state.theme);
+  const dispatch = useDispatch();
+    const { currentUser } = useSelector((state) => state.user);
+    
+    const handleSignout = async () => {
+      try {
+        const res = await fetch(`${BaseURL}auth/signout`, {
+          method: "POST",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+  return (
+    <Navbar
+      className="border-b-2 sticky top-0 z-50 justify-between"
+      fluid
+      rounded
+    >
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <Link
-          to="/"
-          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        <span className="px-2 py-1 bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 rounded-lg text-white">
+          technests.ai
+        </span>
+      </Link>
+      <div className="flex gap-2 md:order-2">
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
         >
-          <span className="px-2 py-1 bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 rounded-lg text-white">
-            technests.ai
-          </span>
-        </Link>
-        <div className="flex gap-2 md:order-2">
-          <Button
-            className="w-12 h-10 hidden sm:inline"
-            color="gray"
-            pill
-            onClick={() => dispatch(toggleTheme())}
+          {theme === "light" ? <FaSun /> : <FaMoon />}
+        </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
           >
-            {theme === "light" ? <FaSun /> : <FaMoon />}
-          </Button>
-          <Button gradientMonochrome="teal">Sign In</Button>
-        </div>
-      </Navbar>
-    );
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientMonochrome="teal">Sign In</Button>
+          </Link>
+        )}
+      </div>
+    </Navbar>
+  );
 }
