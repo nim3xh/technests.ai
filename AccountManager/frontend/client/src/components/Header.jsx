926ad7848419caa815ef342f475f17aaa87f024d
nim3xh <nim3xh@gmail.com>
2024-10-07 02:11:31 +0530

    Refactor Header component to include theme toggle button

--- AccountManager/frontend/client/src/components/Header.jsx
+++ AccountManager/frontend/client/src/components/Header.jsx
@@ -1,29 +1,40 @@
 import { Navbar, TextInput,Button } from "flowbite-react";
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-  return (
-    <Navbar
-      className="border-b-2 sticky top-0 z-50 justify-between"
-      fluid
-      rounded
-    >
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+    const { theme } = useSelector((state) => state.theme);
+    const dispatch = useDispatch();
+    return (
+      <Navbar
+        className="border-b-2 sticky top-0 z-50 justify-between"
+        fluid
+        rounded
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 rounded-lg text-white">
-          technests.ai
-        </span>
-      </Link>
-      <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
-        </Button>
-        <Button gradientMonochrome="teal">Sign In</Button>
-      </div>
-    </Navbar>
-  );
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 rounded-lg text-white">
+            technests.ai
+          </span>
+        </Link>
+        <div className="flex gap-2 md:order-2">
+          <Button
+            className="w-12 h-10 hidden sm:inline"
+            color="gray"
+            pill
+            onClick={() => dispatch(toggleTheme())}
+          >
+            {theme === "light" ? <FaSun /> : <FaMoon />}
+          </Button>
+          <Button gradientMonochrome="teal">Sign In</Button>
+        </div>
+      </Navbar>
+    );
 }
