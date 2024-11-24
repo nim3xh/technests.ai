d57f9f43ee4f8f770761059310847b7692131f41
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2024-11-24 18:10:56 +0530

    Add token verification to TradeType routes for enhanced security

--- AccountManager/api/routes/tradetype.route.js
+++ AccountManager/api/routes/tradetype.route.js
@@ -3,11 +3,11 @@ const tradetypeController = require("../controllers/tradetype.controller");
 const verifyToken = require("../utils/verifyUser");
 const router = express.Router();
 
-router.post("/", tradetypeController.save);
+router.post("/",verifyToken, tradetypeController.save);
 router.get("/", verifyToken, tradetypeController.index);
 router.get("/:id", verifyToken, tradetypeController.show);
 router.put("/:id", verifyToken, tradetypeController.update);
 router.delete("/:id", verifyToken, tradetypeController.destroy);
-router.post("/seed", tradetypeController.savebulk);
+router.post("/seed",verifyToken, tradetypeController.savebulk);
 
 module.exports = router;
