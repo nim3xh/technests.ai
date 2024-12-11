6ef020147d0a0346b433b75e6fa09b8d9d126170
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2024-12-11 20:44:08 +0530

    Refactor DashTradingComp and Header components to improve CSV filtering logic and enhance layout; include header row in filtered results and streamline signout functionality

--- AccountManager/frontend/client/src/components/Header.jsx
+++ AccountManager/frontend/client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -7,58 +7,51 @@ import LogoNew from "../assets/logonew.jpg";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { toggleTheme } from "../redux/theme/themeSlice";
 
+
 const BaseURL = import.meta.env.VITE_BASE_URL;
 
 export default function Header() {
   const { theme } = useSelector((state) => state.theme);
   const dispatch = useDispatch();
-  const { currentUser } = useSelector((state) => state.user);
-
-  const handleSignout = async () => {
-    try {
-      const res = await fetch(`${BaseURL}auth/signout`, {
-        method: "POST",
-      });
-      const data = await res.json();
-      if (!res.ok) {
-        console.log(data.message);
-      } else {
-        dispatch(signoutSuccess());
+    const { currentUser } = useSelector((state) => state.user);
+    
+    const handleSignout = async () => {
+      try {
+        const res = await fetch(`${BaseURL}auth/signout`, {
+          method: "POST",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
       }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+    };
 
   return (
-    <Navbar className="border-b-2 sticky top-0 z-50" fluid rounded>
-      {/* Logo Section */}
-      <div className="flex items-center">
-        <Link
-          to="/dashboard?tab=dash"
-          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-        >
-          <img
-            src={LogoNew}
-            alt="logo"
-            className="w-8 h-8 sm:w-10 sm:h-10 rounded-lg"
-          />
-        </Link>
-      </div>
-
-      {/* Right Section */}
-      <div className="flex items-center gap-2 flex-wrap md:order-2">
-        {/* Theme Toggle Button */}
+    <Navbar
+      className="border-b-2 sticky top-0 z-50 justify-between"
+      fluid
+      rounded
+    >
+      <Link
+        to="/dashboard?tab=dash"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+       < img src={LogoNew} alt="logo" className="w-10 h-10 rounded-lg" />
+      </Link>
+      <div className="flex gap-2 md:order-2">
         <Button
-          className="w-8 h-8 sm:w-12 sm:h-10 hidden sm:block"
+          className="w-12 h-10 hidden sm:inline"
           color="gray"
           pill
           onClick={() => dispatch(toggleTheme())}
         >
           {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
-
-        {/* User Authentication Section */}
         {currentUser ? (
           <Dropdown
             arrowIcon={false}
@@ -70,8 +63,10 @@ export default function Header() {
             <Dropdown.Header>
               <span className="block text-sm">
                 @{currentUser.user.FirstName + " " + currentUser.user.LastName}
+                <br></br>
+                <i>{currentUser.user.email }</i>
               </span>
-              <span className="block text-xs font-medium truncate">
+              <span className="block text-sm font-medium truncate">
                 {currentUser.email}
               </span>
             </Dropdown.Header>
