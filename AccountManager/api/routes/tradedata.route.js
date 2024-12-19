54ebe3638fbe88c75c986aae07cadf8d6c4675c9
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2024-12-19 17:33:40 +0530

    Add index routes for trade, user, and account detail management; update tradeData route to remove token verification for saving and deletion

--- AccountManager/api/routes/tradedata.route.js
+++ AccountManager/api/routes/tradedata.route.js
@@ -5,12 +5,12 @@ const tradeDataController = require('../controllers/tradedata.controller');
 const verifyToken = require('../utils/verifyUser');
 const router = express.Router();
 
-router.post('/', verifyToken, tradeDataController.save);
+router.post('/', tradeDataController.save);
 router.get('/', tradeDataController.index);
 router.get('/getByUserId/:account_number', tradeDataController.tradeDataByAccount);
 router.get('/:id', verifyToken, tradeDataController.show);
 router.patch('/:id', verifyToken, tradeDataController.update);
 router.delete('/delete/:id', verifyToken, tradeDataController.destroy);
-router.delete('/deleteAll', verifyToken, tradeDataController.destroyAll);
+router.delete('/deleteAll', tradeDataController.destroyAll);
 
 module.exports = router;
