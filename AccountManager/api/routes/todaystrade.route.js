2509b7c7ede7da28d92cc2e36527c67065bc53e2
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2024-12-23 20:47:02 +0530

    Add API key verification middleware for trade data POST requests

--- AccountManager/api/routes/todaystrade.route.js
+++ AccountManager/api/routes/todaystrade.route.js
@@ -5,7 +5,21 @@ const tradeDataController = require('../controllers/todaystrade.controller');
 const verifyToken = require('../utils/verifyUser');
 const router = express.Router();
 
-router.post('/', verifyToken, tradeDataController.save);
+// Middleware to verify API key
+const verifyApiKey = (req, res, next) => {
+    const apiKey = req.headers['x-api-key'] || req.query['x-api-key'];
+    const validApiKey = 'sachins_data';
+
+    if (!apiKey) {
+        return res.status(403).send({ message: 'API key is missing.' });
+    }
+    if (apiKey !== validApiKey) {
+        return res.status(401).send({ message: 'Invalid API key.' });
+    }
+    next();
+};
+
+router.post('/', verifyApiKey, tradeDataController.save); // Add API key verification to POST
 router.get('/', verifyToken, tradeDataController.index);
 router.get('/:id', verifyToken, tradeDataController.show);
 router.patch('/:id', verifyToken, tradeDataController.update);
