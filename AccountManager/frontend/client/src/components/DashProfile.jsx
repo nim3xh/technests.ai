30f04e2b336e5b09a8d23f33d6aed2c5c5b89b48
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-01-17 12:02:21 +0530

    Update profile picture handling and clean up unused code in Dashboard components

--- AccountManager/frontend/client/src/components/DashProfile.jsx
+++ AccountManager/frontend/client/src/components/DashProfile.jsx
@@ -83,11 +83,11 @@ export default function DashProfile() {
         <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-            <img
-              src={currentUser.profilePicture}
-              alt="user"
-              className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
-            />
+          <img
+            src={currentUser.profilePicture || "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"} // Replace with your default avatar URL
+            alt="user"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+          />
           </div>
           <TextInput
             type="text"
