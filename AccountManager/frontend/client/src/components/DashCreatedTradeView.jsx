5367249439af317824863db41b4a7aca1810de6e
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-01-21 15:59:40 +0530

    Add loading and error handling to DashCreatedTradeView for non-user roles

--- AccountManager/frontend/client/src/components/DashCreatedTradeView.jsx
+++ AccountManager/frontend/client/src/components/DashCreatedTradeView.jsx
@@ -20,8 +20,16 @@ import {
 import useRealTimeDate from '../hooks/useRealTimeDate';
 import axios from "axios";
 
+const BaseURL = import.meta.env.VITE_BASE_URL;
+
 export default function DashCreatedTradeView() {
+  const { currentUser } = useSelector((state) => state.user);
   const formattedTodayDate = useRealTimeDate();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [trades, setTrades] = useState([]);
+
+
   return (
     <div className="p-3 w-full">
       <Breadcrumb aria-label="Default breadcrumb example">
@@ -34,6 +42,21 @@ export default function DashCreatedTradeView() {
       Created Trades
       </h1>
       <p className="text-lg font-semibold text-gray-600 dark:text-white">{formattedTodayDate}</p> {/* Display the formatted date */}
+      {currentUser.user.role !== "user" && (
+        <>
+          {loading ? (
+            <div className="flex justify-center items-center h-96">
+              <Spinner size="xl" />
+            </div>
+          ) : error ? (
+            <div className="text-red-600">{error}</div>
+          ) :(
+            <>
+            
+            </>
+          )} {/* Display the spinner only if the loading is true */}
+        </>
+      )} {/* Display the table only if the user is not a user */}
     </div>
   )
 }
