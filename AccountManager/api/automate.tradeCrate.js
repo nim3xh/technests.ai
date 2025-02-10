f03083e75dc4d81c0c868df03637b64d0b24c74f
J.M.N.R.D. Jayaweera <105283769+nim3xh@users.noreply.github.com>
2025-02-10 08:29:21 +0530

    Refactor automate.tradeCrate.js to remove data merging functionality and add a test function

--- AccountManager/api/automate.tradeCrate.js
+++ AccountManager/api/automate.tradeCrate.js
@@ -4,33 +4,8 @@ const fs = require('fs');
 
 const BaseURL = "http://localhost:3000/";
 
-// Function to merge users and account details data
-const mergeData = (users, accountDetails) => {
-    return accountDetails.map((account) => {
-      const user = users.find((u) => u.accountNumber === account.accountNumber);
-      return {
-        ...account,
-        name: user ? user.name : "Unknown",
-      };
-    });
+const test = async () => {
+    console.log("Starting test");    
 };
 
-const fetchData = async () => {
-    try {
-      const [usersResponse, accountDetailsResponse] = await Promise.all([
-        axios.get(`${BaseURL}users/index`, {  }),
-        axios.get(`${BaseURL}accountDetails/index`, {  }),
-      ]);
-
-      const mergedData = mergeData(
-        usersResponse.data,
-        accountDetailsResponse.data
-      );
-      
-      combinedData = mergedData;
-
-    } catch (err) {
-      console.error("Error fetching data:", err);
-      console.error("Something went wrong while fetching data.");
-    }
-};
+module.exports = test;
